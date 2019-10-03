739aada890a7714ee93dfde6cb405fc5b41c2465
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-03 14:01:17 +0200

    added empty list

--- main.ts
+++ main.ts
@@ -14,9 +14,15 @@ export let mainProcess = () => {
     if (args.length == 2){
       console.log(emptyContent);
     } else if (args[2] == '-l'){
+        if(content == ""){
+        console.log('No todos for today! :)');
+    } else {
         for (let i: number = 0; i < linesContent.length; i++){
             console.log((i+1) + '. ' + linesContent[i]);
         }
+    } 
+    } if (args[2] == '-a'){
+        
     }
 }
  
