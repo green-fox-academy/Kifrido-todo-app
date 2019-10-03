dd8fc47591c2e126f9244980bd61e477ff8cea79
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-03 11:20:55 +0200

    print usage added

--- main.ts
+++ main.ts
@@ -3,3 +3,13 @@
 const fs = require('fs');
 
 const args: string[] = process.argv;
+
+let mainProcess = () => {
+
+    if (args.length == 2){
+      console.log('Command Line Todo application'+'\n' + '=============================' + '\n' + '\n' + 'Command line arguments:' + '\n' + '-l   Lists all the tasks' + '\n'
+      + '-a   Adds a new task' +'\n' + '-r   Removes a task' + '\n' + '-c   Completes a task');
+    }
+}
+    
+  mainProcess();
